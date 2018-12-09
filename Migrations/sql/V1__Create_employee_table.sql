f28208f30b4582f0e0e56724f679036637013492
Mounarshi Santra <arkaonelovemanu@gmail.com>
2018-12-10 00:55:17 +1100

    data migration, flyaway container done

---
+++ Migrations/sql/V1__Create_employee_table.sql
@@ -0,0 +1,8 @@
+drop table if exists employeeForCRUD;
+
+create table employeeForCRUD (Id int, Name varchar(255), Salary varchar(255), primary key (Id));
+
+insert into employeeForCRUD values (1, 'Jim Brown','10000' );
+insert into employeeForCRUD values (2, 'Jones Taylor','20000' );
+insert into employeeForCRUD values (3, 'Correy Anderson','30000' );
+insert into employeeForCRUD values (4, 'Doug Starr','40000' );

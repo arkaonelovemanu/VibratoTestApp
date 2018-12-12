24e0831e24f4a6832862b154c6523ddfcea7d7a6
Mounarshi Santra <arkaonelovemanu@gmail.com>
2018-12-13 01:01:11 +1100

    added healthcheck

---
+++ Mouna.Api.Crud/Controllers/V1/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mouna.Api.Crud.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    public class HealthController : Controller
+    {
+        // GET: api/Health
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Status healthy");
+        }
+
+        // GET: api/Health/5
+
+    }
+}
